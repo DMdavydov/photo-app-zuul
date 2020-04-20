5acdc5a6821f13f59e8df552f13fe1da192e45fc
DMdavydov <dmitry_davydov1@epam.com>
2020-04-20 13:28:19 +0300

    Initial commit

---
+++ src/main/java/com/ddavydov/bugtrackerzuul/BugTrackerZuulApplication.java
@@ -0,0 +1,18 @@
+package com.ddavydov.bugtrackerzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BugTrackerZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerZuulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddavydov/bugtrackerzuul/BugTrackerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddavydov.bugtrackerzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
