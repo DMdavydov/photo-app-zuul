483440e74c3b7cf8696001f04a4411ba0d637d39
DMdavydov <dmitry_davydov1@epam.com>
2020-05-06 12:23:06 +0300

    security

---
+++ src/main/java/com/ddavydov/bugtrackerzuul/security/WebSecurity.java
@@ -0,0 +1,36 @@
+package com.ddavydov.bugtrackerzuul.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url"))
+                .permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url"))
+                .permitAll()
+                .antMatchers(environment.getProperty("api.h2-console.url"))
+                .permitAll()
+                .anyRequest().authenticated()
+        .and()
+        .addFilter(new AuthorizationFilter(authenticationManager(), environment));
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
